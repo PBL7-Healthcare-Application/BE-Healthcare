6d382fe910fff0257e5a23243fc0129f5543835a
Nguyenquocthanh268 <Nguyenquocthanh268@gmail.com>
2024-04-29 15:50:13 +0700

    feat/HCA-46--Implement-API-Get-User's-Profile

---
+++ Models/ProfileModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BE_Healthcare.Models
+{
+    public class ProfileModel
+    {
+        public string Email { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public DateTime? DOB { get; set; }
+        public bool? Gender { get; set; }
+
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? Avatar { get; set; }
+
+
+    }
+}
---
+++ Services/ProfileRepository.cs
@@ -0,0 +1,47 @@
+﻿using BE_Healthcare.Constant;
+using BE_Healthcare.Data;
+using BE_Healthcare.Models;
+
+namespace BE_Healthcare.Services
+{
+    public class ProfileRepository : IProfileRepository
+    {
+        private readonly MyDbContext _context;
+        private readonly IUserRepository _userRepository;
+
+        public ProfileRepository(MyDbContext context, IUserRepository userRepository)
+        {
+            _context = context;
+            _userRepository = userRepository;
+        }
+
+        public ApiResponse GetPersonalInfo(string email)
+        {
+            if(email == null) 
+            {
+                return new ApiResponse
+                {
+                    StatusCode = StatusCode.FAILED,
+                    Message = AppString.MESSAGE_EMAIL_NULL,
+                };
+            }
+            var info = _userRepository.getUserByEmail(email);
+            var res = new ProfileModel
+            {
+                Email = info.Email,
+                Name = info.Name,
+                DOB = info.DOB,
+                Gender = info.Gender,
+                PhoneNumber = info.PhoneNumber,
+                Address = info.Address,
+                Avatar = info.Avatar,
+            };
+            return new ApiResponse
+            {
+                StatusCode = StatusCode.SUCCESS,
+                Message = AppString.MESSAGE_GETDATA_SUCCESS,
+                Data = res
+            };
+        }
+    }
+}
