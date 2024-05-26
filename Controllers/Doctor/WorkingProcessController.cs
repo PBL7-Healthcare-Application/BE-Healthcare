16d611ffd9af99d225fa993a7b7a0141e8bc4b19
Nguyenquocthanh268 <Nguyenquocthanh268@gmail.com>
2024-05-26 14:13:14 +0700

    feat/HCA-136--Implement-API-Doctor-Add-Certificate-WorkingProcess-TrainingProcess

--- Controllers/Doctor/WorkingProcessController.cs
+++ Controllers/Doctor/WorkingProcessController.cs
@@ -1,4 +1,6 @@
-﻿using BE_Healthcare.Services;
+﻿using BE_Healthcare.Models;
+using BE_Healthcare.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,6 +8,7 @@ namespace BE_Healthcare.Controllers.Doctor
 {
     [Route("api/Doctor/[controller]")]
     [ApiController]
+    [Authorize(Roles = "Doctor")]
     public class WorkingProcessController : ControllerBase
     {
         private readonly IWorkingProcessRepository _workingProcessRepository;
@@ -13,6 +16,27 @@ namespace BE_Healthcare.Controllers.Doctor
         public WorkingProcessController(IWorkingProcessRepository workingProcessRepository)
         {
             _workingProcessRepository = workingProcessRepository;
+        }
+
+        [HttpPost("AddListWorkingProcess")]
+        public IActionResult AddListWorkingProcess(List<AddWorkingProcessModel> model)
+        {
+            try
+            {
+                var checkIdDoctor = User.Claims.FirstOrDefault(u => u.Type == "IdDoctor")?.Value;
+                if (checkIdDoctor == null)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+                }
+                return Ok(_workingProcessRepository.AddListWorkingProcess(Guid.Parse(checkIdDoctor), model));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            }
         }
     }
 }
